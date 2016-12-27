5427c3d3b751adf1a400363ebed83f562a077863
Tom Aldcroft <taldcroft@gmail.com>
2016-12-26 20:29:44 -0500

    Create full package structure and modernize tests

---
+++ Ska/Shell/__init__.py
@@ -0,0 +1,10 @@
+from .shell import *
+from .shell import __version__
+
+
+def test(*args, **kwargs):
+    '''
+    Run py.test unit tests.
+    '''
+    import testr
+    return testr.test(*args, **kwargs)
